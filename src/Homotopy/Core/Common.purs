f1ad8fe106d928a6994e09e26f7e42bfc39b01fd
Lukas Heidemann <lukas@heidemann.me>
2020-06-29 17:59:28 +0200

    Added `Ord` and `Bounded` instance to `SliceIndex`.

--- src/Homotopy/Core/Common.purs
+++ src/Homotopy/Core/Common.purs
@@ -5,7 +5,8 @@ module Homotopy.Core.Common
   , Boundary(..)
   ) where
 
-import Prelude (class Eq, class Ord, compare, (*), (+))
+import Data.Tuple (Tuple(..))
+import Prelude (class Bounded, class Eq, class Ord, compare, (*), (+))
 
 newtype Generator
   = Generator { id :: Int, dimension :: Int }
@@ -30,6 +31,21 @@ instance ordHeight :: Ord Height where
 data SliceIndex
   = Interior Height
   | Boundary Boundary
+
+instance ordSliceIndex :: Ord SliceIndex where
+  compare x y = compare (toTuple x) (toTuple y)
+    where
+    toTuple (Boundary Source) = Tuple 0 0
+
+    toTuple (Interior (Regular i)) = Tuple 1 (i * 2)
+
+    toTuple (Interior (Singular i)) = Tuple 1 (i * 2 + 1)
+
+    toTuple (Boundary Target) = Tuple 2 0
+
+instance boundedSliceIndex :: Bounded SliceIndex where
+  bottom = Boundary Source
+  top = Boundary Target
 
 derive instance eqSliceIndex :: Eq SliceIndex
 
