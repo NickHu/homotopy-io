74a2c723a76153e9fc32c509b7e8dbb748c4bfdf
Nick Hu <me@nickhu.co.uk>
2020-07-01 21:11:49 +0100

    add purty and purescript-language-server to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
 mkShell {
-  buildInputs = [ nodejs purescript spago ];
+  buildInputs = [ nodejs purescript spago ] ++ (with nodePackages; [purty purescript-language-server]);
 
   shellHook = ''
     export PATH="$PWD/node_modules/.bin/:$PATH"
